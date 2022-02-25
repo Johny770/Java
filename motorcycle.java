3fd161787f95324bc9812bf1b2d71ebc3c986d0f
Prisacari Ion <43562887+Johny770@users.noreply.github.com>
2022-02-25 19:25:40 +0200

    Incapsulare

---
+++ motorcycle.java
@@ -0,0 +1,14 @@
+public class Motorcycle {
+
+    // ...
+    private int speed;
+
+    public int getSpeed() {
+        return color;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    // ...
+}
