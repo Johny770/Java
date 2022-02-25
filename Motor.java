458dcd4ac550220147ed01d0d27c345c67fda0dc
Prisacari Ion <43562887+Johny770@users.noreply.github.com>
2022-02-25 19:23:52 +0200

    Moștenire

---
+++ Motor.java
@@ -0,0 +1,16 @@
+public class Motor {
+    private int wheels;
+    private String model;
+    public void start() {
+        // procesul de pornire a motocicletei
+    }
+    
+    public void stop() {
+        // procesul de oprire a motocicletei
+    }
+    
+    public void honk() { 
+        // produce un claxon implicit 
+    }
+
+}
