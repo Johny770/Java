74add52307bed627fdb351aaf5efde5de9aece18
Prisacari Ion <43562887+Johny770@users.noreply.github.com>
2022-02-25 19:23:06 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main  
+{  
+public static void main(int a)  //metoda principala 
+{  
+System.out.println(a);  
+}  
+public static void main(String args[])  
+{     
+System.out.println("metoda principala incoked");  
+main(9);  
+}  
+}  
